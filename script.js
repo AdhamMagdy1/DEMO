bdedd7b1cf081df61607d405950c4f7826cede43
Adham Magdy <99631077+AdhamMagdy1@users.noreply.github.com>
2023-02-15 12:16:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let h = document.querySelector('.shade');
+let h2 = document.querySelector('.main');
+setTimeout(() => (h.classList += ' change'), 400);
+setTimeout(() => (h.classList += ' changecolor'), 1000);
